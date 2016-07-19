b327ac24b828d560050f49e333caa3c02ad006e6
raman <ramanjit456@yahoo.co.in>
2016-07-19 00:22:11 -0400

    completed fibonacci seq

--- fibonacci/fibonacci.asm
+++ fibonacci/fibonacci.asm
@@ -13,15 +13,37 @@ _fibonacci:
 	push	dword ecx
 	push	dword sentence
 	call	_printf
+	xor	eax, eax
 	add	esp, 16	
 	mov	ecx, [ebp+8]
-	xor	ebx, ebx
-	cmp	ebx, ecx
-	jz	cont
-	mov	eax, ecx
+	
+
+	mov	ebx, 1
+	cmp	ecx, ebx
+	jng	cont
+	;mov	eax, ecx
+
+	sub	esp, 12
+	sub	ecx, 1
+	push	dword ecx
+	call	_fibonacci
+	mov	ecx, [esp]
+	;add	eax, ecx
+	add	esp, 16
+	
+	sub     esp, 8
+        sub     ecx, 1
+	push	dword eax
+        push    dword ecx
+        call    _fibonacci
+	mov	ecx, [esp+4]
+        add     eax, ecx
+        add     esp, 16
+	
+
 	jmp	end
 cont:
-	mov	eax, 0		
+	mov	eax, 1		
 end:
 	add	esp, 8
 	pop	ebp
@@ -30,4 +52,5 @@ end:
 
 section .data
 	sentence dd "the param is %d ", 0x0a, 0
+
 
--- fibonacci/main.c
+++ fibonacci/main.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
-
+//1 1 2 3 5 8 13 21
 int fibonacci(int i);
+int fibonacci2(int v);
 
 int main(void){
 	int val;
 	
-	val = fibonacci(10);
-	printf("the value is %d\n", val);
+	val = fibonacci(5);
+	printf("the value of the assembly fibonacci is %d\n", val);
+
+	val = fibonacci2(5);
+	printf("the value of c fibonacci is %d\n", val);
 	return 0;
+}
+
+int fibonacci2(int b){
+	if((b == 0) || (b == 1)){
+		return 1;
+	}
+
+	return fibonacci2(b-1) + fibonacci2(b-2);
 }
